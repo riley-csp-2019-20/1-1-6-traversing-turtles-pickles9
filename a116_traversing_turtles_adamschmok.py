5c41cc3769c039ad68aaa9b885522cd24c41e3ea
pickle <55500841+pickles9@users.noreply.github.com>
2019-09-24 11:17:05 -0400

    Fixed movement code and added color changer

--- a116_traversing_turtles_adamschmok.py
+++ a116_traversing_turtles_adamschmok.py
@@ -11,26 +11,26 @@ turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  color = turtle_colors.pop()
+  t.pencolor(color)
+  t.fillcolor(color)
   my_turtles.append(t)
 
 #  sets coordinate variables
 startx = 0
 starty = 0
-
+count = 1
 #goes right 45 and forward 50 for each turtle
 for t in my_turtles:
     t.speed(1)
     t.penup()
     t.goto(startx, starty)
     t.pendown()
-    t.right(45)     
+    t.right(45 * count)     
     t.forward(50)
-    t.xcor(startx)
-    t.ycor(starty)
-
-#	adds 50 to the start coordinates
-    startx = startx + 50
-    starty = starty + 50
+    startx = t.xcor()
+    starty = t.ycor()
+    count += 1
 
 wn = trtl.Screen()
 wn.mainloop()
